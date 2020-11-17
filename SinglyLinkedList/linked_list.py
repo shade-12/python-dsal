00f4798c01fd83f922040411969705aff61cb2fb
Shade Wong <shadeying@alumni.ubc.ca>
2020-11-16 22:25:00 -0800

    ll

---
+++ SinglyLinkedList/linked_list.py
@@ -0,0 +1,33 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node is not None:
+            end = None if cur_node.next is None else " -> "
+            print(cur_node.data, end = end)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+        last_node = self.head
+        while last_node.next is not None:
+            last_node = last_node.next
+        last_node.next = Node(data)
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+
+
+llist.print_list()  
