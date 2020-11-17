3927f7a9b17d1c8f957e9726d66a5168186d8510
Shade Wong <shadeying@alumni.ubc.ca>
2020-11-16 21:41:58 -0800

    dec to bin

---
+++ Stack/dec_to_bin.py
@@ -0,0 +1,21 @@
+from stack import Stack
+
+'''
+Convert decimal integer to binary
+'''
+
+def dec_to_bin(dec_num):
+    s = Stack()
+    num = dec_num
+
+    while num > 0:
+        s.push(num % 2)
+        num = num // 2
+
+    bin = ''
+    while not s.is_empty():
+        bin += str(s.pop())
+
+    return bin
+
+print(dec_to_bin(8))
