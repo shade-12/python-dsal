14d53e881371424e0c8a0b39bf7534c1c1d09b54
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-05 15:57:53 -0800

    Insertion

--- 2_Singly_Linked_Lists/linked_list.py
+++ 2_Singly_Linked_Lists/linked_list.py
@@ -30,4 +30,9 @@ class LinkedList():
         self.head = new_node
 
     def insert_after_node(self, prev_node, data):
-        
+        if not prev_node:
+            print("Previous node does not exist.")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
