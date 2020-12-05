59c9260070bcbe61edf331b7d209bddeb64e55b3
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-05 15:51:57 -0800

    Redo

--- 2_Singly_Linked_Lists/linked_list.py
+++ 2_Singly_Linked_Lists/linked_list.py
@@ -7,3 +7,27 @@ class Node():
 class LinkedList():
     def __init__(self):
         self.head = None
+
+    def print_list(self):
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data, end=" -> ")
+            curr_node = curr_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            curr_node = self.head
+            while curr_node.next is not None:
+                curr_node = curr_node.next
+            curr_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, data):
+        
