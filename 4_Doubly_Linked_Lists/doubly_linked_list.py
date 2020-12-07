7549a73c9e2af453f053ea9e423e830e94912e27
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-06 23:18:29 -0800

    Basic funs

--- 4_Doubly_Linked_Lists/doubly_linked_list.py
+++ 4_Doubly_Linked_Lists/doubly_linked_list.py
@@ -9,14 +9,41 @@ class DoublyLinkedList():
         self.head = None
 
     def print_list(self):
-        pass
+        curr = self.head
+        while curr:
+            end = None if not curr.next else " -> "
+            print(curr.data, end=end)
+            curr = curr.next
     
     ########## START: APPEND/PREPEND ##########
-    
-    def append(self, data):
-        pass
 
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = new_node
+            new_node.prev = curr
+        
     def prepend(self, data):
-        pass
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head.prev = new_node
+        self.head = new_node
 
     ########## END: APPEND/PREPEND ##########
+
+
+dllist = DoublyLinkedList()
+dllist.prepend(0)
+dllist.append(1)
+dllist.append(2)
+dllist.append(3)
+dllist.append(4)
+dllist.prepend(5)
+
+dllist.print_list()
