4cc5c12e8d4c4021faf5378916ad6c5208f0cb6e
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-06 23:53:51 -0800

    Add b/a

--- 4_Doubly_Linked_Lists/doubly_linked_list.py
+++ 4_Doubly_Linked_Lists/doubly_linked_list.py
@@ -18,6 +18,9 @@ class DoublyLinkedList():
     ########## START: APPEND/PREPEND ##########
 
     def append(self, data):
+        """
+        Insert new node at the end of linked list.
+        """
         new_node = Node(data)
         if not self.head:
             self.head = new_node
@@ -29,6 +32,9 @@ class DoublyLinkedList():
             new_node.prev = curr
         
     def prepend(self, data):
+        """
+        Insert new node at the head of linked list.
+        """
         new_node = Node(data)
         if self.head:
             new_node.next = self.head
@@ -37,13 +43,61 @@ class DoublyLinkedList():
 
     ########## END: APPEND/PREPEND ##########
 
+    ########## START: INSERT NODE ##########
+
+    def add_after_node(self, key, data):
+        """
+        Insert new node after the node where node.data = key.
+        Assume elements within linked list are unique.
+        """
+        if not self.head:
+            return
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                new_node = Node(data)
+                new_node.next = curr.next
+                new_node.prev = curr
+                curr.next = new_node
+                if new_node.next:
+                    new_node.next.prev = new_node
+                return
+            curr = curr.next
+
+    def add_before_node(self, key, data):
+        """
+        Insert new node before the node where node.data = key.
+        Assume elements within linked list are unique.
+        """
+        if not self.head:
+            return
+        if self.head.data == key:
+            self.prepend(data)
+            return
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                new_node = Node(data)
+                new_node.next = curr
+                new_node.prev = curr.prev
+                curr.prev = new_node
+                if new_node.prev:
+                    new_node.prev.next = new_node
+                return
+            curr = curr.next
+
+    ########## END: INSERT NODE ##########
+
 
 dllist = DoublyLinkedList()
+
 dllist.prepend(0)
 dllist.append(1)
 dllist.append(2)
 dllist.append(3)
 dllist.append(4)
 dllist.prepend(5)
+dllist.add_after_node(4,6)
+dllist.add_before_node(5,9)
 
 dllist.print_list()
