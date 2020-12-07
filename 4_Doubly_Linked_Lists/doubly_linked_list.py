fe2aa2bf07643a0c8ca28c0a12548772d6982e7e
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-06 23:02:19 -0800

    init

---
+++ 4_Doubly_Linked_Lists/doubly_linked_list.py
@@ -0,0 +1,22 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList():
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        pass
+    
+    ########## START: APPEND/PREPEND ##########
+    
+    def append(self, data):
+        pass
+
+    def prepend(self, data):
+        pass
+
+    ########## END: APPEND/PREPEND ##########
