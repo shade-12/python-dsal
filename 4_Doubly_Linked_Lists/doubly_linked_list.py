cd605c0072dc03afbff804cd9f770ebd037c7c03
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-09 22:44:55 -0800

    Rm dup

--- 4_Doubly_Linked_Lists/doubly_linked_list.py
+++ 4_Doubly_Linked_Lists/doubly_linked_list.py
@@ -90,7 +90,7 @@ class DoublyLinkedList():
 
     ########## START: DELETE NODE ##########
     
-    def delete(self, key):
+    def delete_key(self, key):
         """
         Remove a node where node.data = key from linked list.
         """
@@ -127,6 +127,43 @@ class DoublyLinkedList():
             curr = None
         return
 
+    def delete_node(self, node):
+        """
+        Remove node from linked list if node exist in the list.
+        """
+        if not self.head:
+            return
+
+        if self.head == node:
+            # Case 1: Remove the only node from the list
+            if not self.head.next:
+                self.head = None
+            # Case 2: Remove head node
+            else:
+                new_head = self.head.next
+                new_head.prev = None
+                self.head = None
+                self.head = new_head
+            return
+
+        # Case 3: Remove node where node.next is not None
+        curr = self.head.next
+        while curr.next:
+            if curr == node:
+                curr.prev.next = curr.next
+                curr.next.prev = curr.prev
+                curr.next, curr.prev = None, None
+                curr = None
+                return
+            curr = curr.next
+
+        # Case 4: Remove node where node.next is None
+        if curr == node:
+            curr.prev.next = None
+            curr.next, curr.prev = None, None
+            curr = None
+        return
+
     ########## END: DELETE NODE ##########
 
     ########## START: REVERSE ##########
@@ -148,6 +185,30 @@ class DoublyLinkedList():
         self.head = curr
 
     ########## END: REVERSE ##########
+
+    ########## START: REMOVE DUPLICATES ##########
+
+    def remove_duplicates(self):
+        """
+        Remove all duplicates from doubly linked list.
+        """
+        # Return if linked list has length < 2
+        if not self.head or not self.head.next:
+            return
+
+        store = set()
+        curr = self.head
+        while curr:
+            if not curr.data in store:
+                store.add(curr.data)
+                curr = curr.next
+            else:
+                nxt = curr.next
+                self.delete_node(curr)
+                curr = nxt
+            
+
+    ########## END: REMOVE DUPLICATES ##########
 
 
 dllist = DoublyLinkedList()
