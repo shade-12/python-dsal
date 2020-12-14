7642673a9935eb4005755bd6c5a87a89d80712fd
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-13 16:11:32 -0800

    bst

---
+++ 7_Binary_Search_Trees/bst.py
@@ -0,0 +1,10 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
