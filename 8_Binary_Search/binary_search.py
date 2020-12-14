6dfed78cc8de5778ca751986ba6f3d1a307392b8
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-13 21:33:47 -0800

    bs recur

--- 8_Binary_Search/binary_search.py
+++ 8_Binary_Search/binary_search.py
@@ -21,11 +21,20 @@ def binary_search_recursive(data, target, low, high):
     Find target element within data array recursively.
     Precondition: data is sorted
     """
-
+    if high < low:
+        return False
+    middle = low + (high - low) // 2
+    val = data[middle]
+    if target == val:
+        return True
+    elif target < val:
+        return binary_search_recursive(data, target, low, middle - 1)
+    else:
+        return binary_search_recursive(data, target, middle + 1, high)
 
 
 data = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,37]
-target = 7
+target = 10
 
-# print(binary_search_recursive(data, target, 0, len(data)-1))
-print(binary_search_iterative(data, target))
+print(binary_search_recursive(data, target, 0, len(data)-1))
+# print(binary_search_iterative(data, target))
